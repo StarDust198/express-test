94d75995f16d7f8b0209d5137edceb4315360d92
Sergei Zhilinskij <roadtomars2030@gmail.com>
2023-07-05 11:27:54 +0400

    add express router, spread requests, sends html files now

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const http = require('http');
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const server = http.createServer(app);
+app.use('/admin', adminRouter);
+app.use(shopRouter);
 
-server.listen(3000);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
