75c31a12293ae378c845733bb72050e284bd2eed
Sergei Zhilinskij <roadtomars2030@gmail.com>
2023-07-11 10:34:45 +0400

    switch to ejs, cleanup

--- app.js
+++ app.js
@@ -2,18 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine(
-  'handlebars', // sets the extention for files
-  expressHbs.engine({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-  })
-);
-app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs'); // Looks for 'ejs' extensions
 app.set('views', 'views'); // Already set to "views" by default
 
 const adminRouter = require('./routes/admin').router;
@@ -21,13 +13,13 @@ const shopRouter = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-// Checks for request extensitions and looks for them in public
+// Checks for request with extensitions and looks for this files in public
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.render('404', { docTitle: 'Page not found' });
+  res.render('404', { docTitle: 'Page not found', path: '' });
 });
 
 app.listen(3000);
