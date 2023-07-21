7a85f975d6e62486bc246d871e10134d05206605
Sergei Zhilinskij <roadtomars2030@gmail.com>
2023-07-21 11:31:29 +0400

    start adding sequelize

--- helpers/database.js
+++ helpers/database.js
@@ -1,11 +1,10 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
+
 const { password } = require('../.env');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', password, {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password,
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
--- models/Product.js
+++ models/Product.js
@@ -1,40 +1,72 @@
-const db = require('../helpers/database');
+const Sequelize = require('sequelize');
 
-const Cart = require('./Cart');
+const sequelize = require('../util/database');
 
-const getProductsFromFile = (cb) => {
-  fs.readFile(productsFile, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  // title: Sequelize.STRING, -- also possible way
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+module.exports = Product;
 
-  save() {
-    return db.execute(
-      'INSERT INTO products (title, price, imageurl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
+// const db = require('../helpers/database');
 
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
+// const Cart = require('./Cart');
 
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
+// const getProductsFromFile = (cb) => {
+//   fs.readFile(productsFile, (err, fileContent) => {
+//     if (err) {
+//       cb([]);
+//     } else {
+//       cb(JSON.parse(fileContent));
+//     }
+//   });
+// };
 
-  static deleteById(id) {}
-};
+// module.exports = class Product {
+//   constructor(id, title, imageUrl, description, price) {
+//     this.id = id;
+//     this.title = title;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//     this.price = price;
+//   }
+
+//   save() {
+//     return db.execute(
+//       'INSERT INTO products (title, price, imageurl, description) VALUES (?, ?, ?, ?)',
+//       [this.title, this.price, this.imageUrl, this.description]
+//     );
+//   }
+
+//   static fetchAll() {
+//     return db.execute('SELECT * FROM products');
+//   }
+
+//   static findById(id) {
+//     return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
+//   }
+
+//   static deleteById(id) {}
+// };
