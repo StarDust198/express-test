11ea85f9031d97b7b18c767a035ff7c34db34e75
Sergei Zhilinskij <roadtomars2030@gmail.com>
2023-11-14 22:41:51 +0300

    add stripe payments

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const stripeKey = require('../.env').stripeKey;
+const stripe = require('stripe')(stripeKey);
 
 const PDFDocument = require('pdfkit');
 
@@ -134,6 +136,80 @@ exports.postCartDelete = (req, res, next) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
+    });
+};
+
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
+
+  req.user
+    .populate('cart.items.productId')
+    .then((user) => {
+      products = user.cart.items;
+      total = products.reduce(
+        (acc, product) => acc + product.quantity * product.productId.price,
+        0
+      );
+
+      return stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: products.map((p) => {
+          return {
+            price_data: {
+              unit_amount: p.productId.price * 100,
+              product_data: {
+                name: p.productId.title,
+                description: p.productId.description,
+              },
+              currency: 'usd',
+            },
+            quantity: p.quantity,
+          };
+        }),
+        mode: 'payment',
+        success_url: `${req.protocol}://${req.get('host')}/checkout/success`,
+        cancel_url: `${req.protocol}://${req.get('host')}/checkout/cancel`,
+      });
+    })
+    .then((session) => {
+      return res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout',
+        products,
+        totalSum: total,
+        sessionId: session.id,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .then((user) => {
+      const order = new Order({
+        user: {
+          email: user.email,
+          userId: user,
+        },
+        items: user.cart.items,
+      });
+
+      return order.save();
+    })
+    .then(() => {
+      return req.user.clearCart();
+    })
+    .then((result) => {
+      res.redirect('/orders');
+    })
+    .catch((err) => {
+      console.log('ERROR ADDING ORDER: ', err);
     });
 };
 
